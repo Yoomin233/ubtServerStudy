d031858974d053e4fdf6edad6b7250e2c56ca913
ShiDongjie <dongjie.shi@baidao.com>
2017-02-21 09:28:18 +0800

    save

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ var express     = require("express"),
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://192.168.19.172:27017/ubt', {}, function (err, res) {
-    if (err) { 
-        console.log('Connection refused to mongodb://192.168.19.172:27017/ubt');
-        console.log(err);
-    } else {
-        console.log('Connection successful to: mongodb://192.168.19.172:27017/ubt');
-    }
+  if (err) { 
+      console.log('Connection refused to mongodb://192.168.19.172:27017/ubt');
+      console.log(err);
+  } else {
+      console.log('Connection successful to: mongodb://192.168.19.172:27017/ubt');
+  }
 });
 var Schema = mongoose.Schema;
 var PV = new Schema({ 
-    pv: Schema.Types.Mixed 
+  pv: Schema.Types.Mixed 
 });
 var pvModel   = mongoose.model('PV', PV);
 
 
 var app = express();
-
 app.use(bodyParser.json({limit: '10mb'}));
 app.use(bodyParser.urlencoded({ extended: true,limit: '10mb', parameterLimit:50 }));
 
@@ -41,27 +40,26 @@ app.get("/healthcheck", function(req,res){
 });
 
 app.get("/ubt/pv.gif", function(req, res) {
-    try {
-        var queryStr=require('url').parse(req.url).query || '';
-
-        if (queryStr == '') {
-            return res.sendStatus(400);
-        }
-        console.log(decodeURIComponent(queryStr));
-        var pvData = JSON.parse(decodeURIComponent(queryStr));
-        var pv = new pvModel();
-        pv.pv=pvData;
-        pv.save(function(err) {
-          if (err) {
-            console.log(err);
-            return res.sendStatus(500);
-          } 
-          return res.sendStatus(200);
-        });
-    } catch (e) {
-    	console.log(e)
-        return res.sendStatus(500);
+  try {
+    var queryStr=require('url').parse(req.url).query || '';
+    if (queryStr == '') {
+        return res.sendStatus(400);
     }
+    console.log(decodeURIComponent(queryStr));
+    var pvData = JSON.parse(decodeURIComponent(queryStr));
+    var pv = new pvModel();
+    pv.pv=pvData;
+    pv.save(function(err) {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(500);
+      } 
+      return res.sendStatus(200);
+    });
+  } catch (e) {
+  	console.log(e)
+    return res.sendStatus(500);
+  }
 });
 
 var port = process.env.NODE_PORT || 7033;
